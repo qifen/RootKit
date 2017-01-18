5e3e4221e07e6a285c060db2139f581ff1218ab0
sx14 <1059363093@qq.com>
2017-01-18 23:29:07 +0800

    图生成算法完成

--- app/src/main/java/com/wei/rootkit/logAnalysis/DynamicAnalyzer.java
+++ app/src/main/java/com/wei/rootkit/logAnalysis/DynamicAnalyzer.java
@@ -1,7 +1,15 @@
 package com.wei.rootkit.logAnalysis;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by xu on 2017/1/18.
@@ -15,8 +23,46 @@ class DynamicAnalyzer {
      * 实现图生成算法，将原始日志信息转换成为初级行为图
      * 生成初级行为图：树形节点序列，节点内部保存节点之间的关系
      */
-    private void getPrimaryGraph(String logPath){
+    private void getPrimaryGraph(String logPath) throws FileNotFoundException {
         List<String> logList = getLog(logPath);
+        List<Node> rawNodes = new ArrayList<>();
+        rawNodes.add(new Node());//root
+        for (int logIndex = 1; logIndex < logList.size() ; logIndex++){
+            Node n = logParser(logList.get(logIndex));
+            n.setIndex(logIndex);
+            rawNodes.add(n);
+        }
+        Map<Integer,Map<Integer,Node>> map = new HashMap<>();
+        String cloneFunc = "clone";
+        for (int nodeIndex = 1 ; nodeIndex < rawNodes.size() ; nodeIndex ++){//遍历每一行日志
+            Node node = rawNodes.get(nodeIndex);
+            if (map.containsKey(node.getUid())){//应用程序节点存在
+                Map<Integer,Node> m = map.get(node.getUid());
+                if (m.containsKey(node.getPid())){//进程节点存在
+                    if (node.getFunc().equals(cloneFunc)){//clone
+                        int cid = Integer.parseInt(node.getParam());
+                        node.setCid(cid);
+                        Node parent = m.get(node.getPid());
+                        node.setParent(parent.getIndex());
+                        parent.setChild(parent.getChild()+1);
+                        m.put(cid,node);
+                    }else {
+                        Node parent = m.get(node.getPid());
+                        node.setParent(parent.getIndex());
+                        m.remove(node.getPid());//将当前的树叶节点移除
+                        m.put(node.getPid(),node);
+                        //将最新的动作（当前node）做为最新的树叶节点
+                        //也就是说，后续的动作跟在当前node后面
+                    }
+                }else {//某应用的第一个进程
+                    m.put(node.getPid(),node);
+                }
+            }else {//第一个应用程序节点
+                Map<Integer,Node> m = new HashMap<>();
+                m.put(node.getPid(),node);
+                map.put(node.getUid(),m);
+            }
+        }
     }
 
     /**
@@ -38,8 +84,19 @@ class DynamicAnalyzer {
      * @param logPath
      * @return 一条一条的日志信息
      */
-    private List<String> getLog(String logPath){
-        return null;
+    private List<String> getLog(String logPath) throws FileNotFoundException {
+        List<String> logList = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(logPath));
+        String log;
+        try {
+            while ((log = reader.readLine()) != null){
+                logList.add(log);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return logList;
     }
 
     /**
@@ -48,10 +105,32 @@ class DynamicAnalyzer {
      * @return 图节点
      */
     private Node logParser(String log){
+        String regexPattern = "\\[Rootkit\\]\\((\\d+)\\)\\((\\d+)\\)(\\w+)\\((.*)\\)";
+        Pattern pattern = Pattern.compile(regexPattern);
+        Matcher matcher = pattern.matcher(log);
+        if (matcher.find()){
+            int uid = Integer.parseInt(matcher.group(1));
+            int pid = Integer.parseInt(matcher.group(2));
+            String method = matcher.group(3);
+            String param = matcher.group(4);
+            Node n = new Node();
+            n.setUid(uid);
+            n.setPid(pid);
+            n.setFunc(method);
+            n.setParam(param);
+            return n;
+        }
         return null;
     }
 
-    public Node[] getGraph(String logPath){
+//    public static void main(String[] args) {
+//        String log = "[Rootkit](954)(10074)finishReceiver(abortBroadcast = 0)";
+//        DynamicAnalyzer analyzer = new DynamicAnalyzer();
+//        Node node = analyzer.logParser(log);
+//        System.out.println(node);
+//    }
+
+    public Node[] getGraph(String logPath) throws FileNotFoundException {
         getPrimaryGraph(logPath);
         matchBroadCastLifeCycle();
         simplifyGraph();
--- app/src/main/java/com/wei/rootkit/logAnalysis/Node.java
+++ app/src/main/java/com/wei/rootkit/logAnalysis/Node.java
@@ -7,5 +7,102 @@ package com.wei.rootkit.logAnalysis;
  *
  */
 class Node {
-    //todo 定义节点
+    private int index;
+    private int pid, uid;
+    private int cid;
+    private String func;
+    private String param;
+
+    private int parent;
+    private int next;
+    private int child;
+
+    public String toString(){
+        String s = "";
+        s += ("pid:" + pid + "\n");
+        s += ("cid:" + cid + "\n");
+        s += ("method:" + func + "\n");
+        s += ("param:" + param + "\n");
+        return s;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getFunc() {
+        return func;
+    }
+
+    public void setFunc(String func) {
+        this.func = func;
+    }
+
+    public int getParent() {
+        return parent;
+    }
+
+    public void setParent(int parent) {
+        this.parent = parent;
+    }
+
+    public int getNext() {
+        return next;
+    }
+
+    public void setNext(int next) {
+        this.next = next;
+    }
+
+    public int getChild() {
+        return child;
+    }
+
+    public void setChild(int child) {
+        this.child = child;
+    }
+
+    public String getParam() {
+        return param;
+    }
+
+    public void setParam(String param) {
+        this.param = param;
+    }
+
+    //    后面的暂时还不知道干嘛用的
+//    int scheduleFunc;
+//    int matchid;
+//    int finishFunc;
+//
+//    int app;
+//    int visit;
 }
